d9f19c6b1974e8b73dde82e7d97139c99c66483a
Tanner Oliver <435.tanner@gmail.com>
2021-09-22 19:36:28 -0500

    adding questions and answers and some more of the js code

--- assets/script.js
+++ assets/script.js
@@ -14,9 +14,9 @@ THEN I can save my initials and my score
 
 I have a bunch of quiz questions to ask. What is the best way to store all those questions, PLUS the correct answer for each one?
 For each question in the quiz:
-    The question itself
-    The possible answers 
-    Which answer is correct 
+    -The question itself
+    -The possible answers 
+    -Which answer is correct 
 
 Have a process where:
     When the game starts, a countdown begins
@@ -32,3 +32,48 @@ After all questions OR after time runs out, show the user their score
 High score tracking
 
 */
+var questions = [
+  {
+    questionText: "5 + 5 =",
+    choices: ["1) 5", "2) 10", "3) 15", "4) 11"],
+    answer: "2) 10",
+  },
+
+  {
+    questionText: "5 - 5 =",
+    choices: ["1) 0", "2) 5", "3) 1", "4) 7"],
+    answer: "1) 0",
+  },
+
+  {
+    questionText: "5 x 5 =",
+    choices: ["1) 10", "2) 15", "3) 50", "4) 25"],
+    answer: "4) 25",
+  },
+
+  {
+    questionText: "5 / 5 =",
+    choices: ["1) 10", "2) 27", "3) 1", "4) 35"],
+    answer: "3) 1",
+  },
+];
+
+var timeEl = document.querySelector(".countdown");
+
+var secondsLeft = 60;
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left.";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      //insert function for ending of quiz here
+    }
+  }, 1000);
+}
+function endQuiz() {}
+//Button.addEventListener("click", function() {
+//setTime()
+//})
